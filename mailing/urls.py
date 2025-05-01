27c060d9ecb5ee9fefc96ade19a2a6ca5734c4ec
Kirill_Medny <medny.kirill@yandex.ru>
2025-05-01 21:02:33 +0300

    crud for client and mailing

--- mailing/urls.py
+++ mailing/urls.py
@@ -5,14 +5,33 @@ from mailing.views import (
     MessageCreateView,
     MessageUpdateView,
     MessageDeleteView,
+    ClientListView,
+    ClientCreateView,
+    ClientUpdateView,
+    ClientDeleteView,
+    MailingListView,
+    MailingCreateView,
+    MailingUpdateView,
+    MailingDeleteView,
 )
 
 app_name = 'mailing'
 
 urlpatterns = [
     path('', home, name='home'),
+
     path('messages/', MessageListView.as_view(), name='message_list'),
     path('messages/create/', MessageCreateView.as_view(), name='message_create'),
     path('messages/update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
     path('messages/delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
+
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('clients/create/', ClientCreateView.as_view(), name='client_create'),
+    path('clients/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
+    path('clients/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
+
+    path('mailings/', MailingListView.as_view(), name='mailing_list'),
+    path('mailings/create/', MailingCreateView.as_view(), name='mailing_create'),
+    path('mailings/update/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailings/delete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -75,3 +75,115 @@ class MessageDeleteView(LoginRequiredMixin, DeleteView):
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, 'Сообщение успешно удалено.')
         return super().delete(request, *args, **kwargs)
+
+
+class ClientListView(LoginRequiredMixin, ListView):
+    model = Client
+    template_name = 'mailing/client_list.html'
+    context_object_name = 'clients'
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    model = Client
+    template_name = 'mailing/client_form.html'
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('mailing:client_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        messages.success(self.request, 'Клиент успешно создан.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ошибка создания клиента. Проверьте введенные данные.')
+        return super().form_invalid(form)
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    template_name = 'mailing/client_form.html'
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('mailing:client_list')
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Клиент успешно обновлен.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ошибка обновления клиента. Проверьте введенные данные.')
+        return super().form_invalid(form)
+
+
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Client
+    template_name = 'mailing/client_confirm_delete.html'
+    success_url = reverse_lazy('mailing:client_list')
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+    def delete(self, request, *args, **kwargs):
+         messages.success(self.request, 'Клиент успешно удален.')
+         return super().delete(request, *args, **kwargs)
+
+
+class MailingListView(LoginRequiredMixin, ListView):
+    model = Mailing
+    template_name = 'mailing/mailing_list.html'
+    context_object_name = 'mailings'
+
+    def get_queryset(self):
+        return Mailing.objects.filter(owner=self.request.user)
+
+
+class MailingCreateView(LoginRequiredMixin, CreateView):
+    model = Mailing
+    template_name = 'mailing/mailing_form.html'
+    fields = ['start_time', 'end_time', 'status', 'message', 'clients']
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        messages.success(self.request, 'Рассылка успешно создана.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ошибка создания рассылки. Проверьте введенные данные.')
+        return super().form_invalid(form)
+
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
+    model = Mailing
+    template_name = 'mailing/mailing_form.html'
+    fields = ['start_time', 'end_time', 'status', 'message', 'clients']
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def get_queryset(self):
+        return Mailing.objects.filter(owner=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Рассылка успешно обновлена.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ошибка обновления рассылки. Проверьте введенные данные.')
+        return super().form_invalid(form)
+
+
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
+    model = Mailing
+    template_name = 'mailing/mailing_confirm_delete.html'
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def get_queryset(self):
+        return Mailing.objects.filter(owner=self.request.user)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Рассылка успешно удалена.')
+        return super().delete(request, *args, **kwargs)
